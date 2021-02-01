ce900b7ca6ea8e1ea946f812ccd48fc94217d3e9
Carlos Fernandes <carlosamfernandes@gmail.com>
2021-02-01 20:13:16 -0300

    Starting project

---
+++ terminal-register/src/main/java/br/com/carlos/terminalregister/TerminalRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlos.terminalregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalRegisterApplication.class, args);
+	}
+
+}
